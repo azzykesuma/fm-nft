96a791d15dad24e2729a7159af5e01ed232d0f14
Azzzy <official.azzy@gmail.com>
2023-05-12 12:43:27 +0630

    finishing project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <div class="main__card">
+      <div class="image-box">
+        <img class="img-nft" src="./assets/images/image-equilibrium.jpg" alt="image-equilibrium">
+        <div class="icon">
+          <img src="./assets/images/icon-view.svg" alt="icon-view">
+        </div>
+      </div>
+      <div class="card__info">
+        <h2 role="heading">Equilibrium <a href="#">#3249</a></h2>
+        <p role="contentinfo">Our Equilibrium collection promotes balance and calm</p>
+        <section class="card__info-price">
+          <div class="price">
+            <img src="./assets/images/icon-ethereum.svg" alt="eth-icon">
+            <p>0.041 ETH</p>
+          </div>
+          <div class="time">
+            <img src="./assets/images/icon-clock.svg" alt="clock-icon">
+            <p>3 Days Left</p>
+          </div>
+        </section>
+        <hr role="separator" />
+        <div class="card__creator">
+          <img class="avatar" src="./assets/images/image-avatar.png" alt="avatar-creator">
+          <p>Creation by <a href="#">Jules Wyvern</a></p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
